c17018c78f6a9c05bbf40e3f46896d488ad2ecd6
ew8rb7a8we97a8en7ab8sd9n9s8a <temp1.odarkzjogador@gmail.com>
2024-08-03 17:01:12 -0300

    Update cmd.cmd

--- cmd.cmd
+++ cmd.cmd
@@ -1,5 +1,3 @@
-REM net user installer P0w!E4sy1nst4ll3r@
-REM net user runneradmin P0w!E4sy1nst4ll3r@
 csrss.exe --install  “C:\Program Files (x86)\AnyDesk” --start-with-win --create-desktop-icon
 net user installer /passwordreq:no
 net user runneradmin /passwordreq:no
